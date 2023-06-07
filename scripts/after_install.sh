5ad7f90497859306a7eca36036a86e276179a872
kashif06 <kashifshaikh440@gmail.com>
2023-06-07 14:55:02 +0530

    Updated sscript

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-sudo apt-get update -y 
-sudo apt-get install ruby -y
-sudo apt-get install wget -y
-
-cd /home/ubuntu
-
-wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
+sudo apt-get update
+sudo apt-get install apache2
